2e9059dfba7f08da2cc6b4d900aee09254f60b96
RACCNOX <alan.guillet2610@gmail.com>
2025-12-17 18:00:30 +0100

    Compétences

--- compétences/script.js
+++ compétences/script.js
@@ -1,7 +1,6 @@
 const canvas = document.getElementById("matrixCanvas");
 const ctx = canvas.getContext("2d");
 
-// Fonction de redimensionnement
 function resizeCanvas() {
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
@@ -24,19 +23,16 @@ window.addEventListener('resize', initDrops);
 function draw() {
     ctx.fillStyle = "rgba(0, 0, 0, 0.1)";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
-
     ctx.fillStyle = "#0f0";
     ctx.font = `${fontSize}px monospace`;
 
     for (let i = 0; i < drops.length; i++) {
         const text = letters[Math.floor(Math.random() * letters.length)];
         ctx.fillText(text, i * fontSize, drops[i] * fontSize);
-
         if (drops[i] * fontSize > canvas.height && Math.random() > 0.975) {
             drops[i] = 0;
         }
         drops[i]++;
     }
 }
-
 setInterval(draw, 50);
